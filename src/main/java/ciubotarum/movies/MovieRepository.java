7d9e0f0052085d26d43300253c19b86e505df7bc
ciubotarum <ciubotarum@outlook.com>
2024-07-25 13:58:59 +0300

    Created Repositories

---
+++ src/main/java/ciubotarum/movies/MovieRepository.java
@@ -0,0 +1,13 @@
+package ciubotarum.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+    // Use Optional because it can return null
+    Optional<Movie> findMovieByImdbId(String imdbId);
+}
