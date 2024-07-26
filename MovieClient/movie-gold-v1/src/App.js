aa75af194565f97086399c1101bb7e2686f43358
ciubotarum <ciubotarum@outlook.com>
2024-07-26 12:13:32 +0300

    Added routes and functionality for App function

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -5,10 +5,15 @@ import Layout from './components/Layout';
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState([]);
 
   const getMovies = async () => {
 
@@ -20,7 +25,22 @@ function App() {
     } catch(err) {
       console.log(err);
     }
+  }
 
+  const getMovieData = async (movieId) => {
+
+    try{
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+
+    } catch(error) {
+        console.error(error);
+    }
 
   }
 
@@ -34,6 +54,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route path='/' element={<Home movies={movies}/>} ></Route>
+          <Route path='/Trailer/:ytTrailerId' element={<Trailer/>}></Route>
+          <Route path='/Reviews/:movieId' element={<Reviews getMovieData = {getMovieData} movie={movie} reviews={reviews} setReviews={setReviews} />}></Route>
+          <Route path='*' element={<NotFound/>}></Route>
         </Route>
       </Routes>
 
