60772e4d3a8859b0e1f9351ec2b8ddf01a6484a0
ciubotarum <ciubotarum@outlook.com>
2024-07-26 09:40:20 +0300

    Created Carousel for Web Page

---
+++ MovieClient/movie-gold-v1/src/components/hero/Hero.js
@@ -0,0 +1,40 @@
+import './Hero.css';
+import Carousel from "react-material-ui-carousel";
+import { Paper } from "@mui/material";
+
+const Hero = ({movies = [] }) => {
+    if (!movies.length) {
+        return <div>No movies available</div>;
+    }
+
+    return (
+        <div className="movie-carousel-container">
+            <Carousel>
+                {
+                    movies.map((movie) => {
+                        return (
+                            <Paper>
+                                <div className="movie-card-container">
+                                    <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
+                                        <div className="movie-detail">
+                                            <div className="movie-poster">
+                                                <img src={movie.poster} alt="" />
+                                            </div>
+                                            <div className="movie-title">
+                                                <h4>{movie.title}</h4>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </Paper>
+
+                        )
+                    })
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+export default Hero
