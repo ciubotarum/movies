1f7987a1249494753c8f819ab85472e47a405782
ciubotarum <ciubotarum@outlook.com>
2024-07-26 12:14:46 +0300

    Added trailer links to be displayed

--- MovieClient/movie-gold-v1/src/components/hero/Hero.js
+++ MovieClient/movie-gold-v1/src/components/hero/Hero.js
@@ -1,10 +1,17 @@
 import './Hero.css';
 import Carousel from "react-material-ui-carousel";
 import { Paper } from "@mui/material";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import { Link, useNavigate } from "react-router-dom";
+import  Button  from 'react-bootstrap/Button';
 
 const Hero = ({movies = [] }) => {
-    if (!movies.length) {
-        return <div>No movies available</div>;
+    
+    const navigate = useNavigate();
+
+    function reviews(movieId) {
+        navigate(`/Reviews/${movieId}`);
     }
 
     return (
@@ -13,7 +20,7 @@ const Hero = ({movies = [] }) => {
                 {
                     movies.map((movie) => {
                         return (
-                            <Paper>
+                            <Paper key={movie.imdbId}>
                                 <div className="movie-card-container">
                                     <div className="movie-card" style={{"--img": `url(${movie.backdrops[0]})`}}>
                                         <div className="movie-detail">
@@ -22,6 +29,19 @@ const Hero = ({movies = [] }) => {
                                             </div>
                                             <div className="movie-title">
                                                 <h4>{movie.title}</h4>
+                                            </div>
+                                            <div className='movie-buttons-container'>
+                                                <Link to={`/Trailer/${movie.trailerLink.substring(movie.trailerLink.length - 11)}`}>
+                                                <div className='play-button-icon-container'>
+                                                    <FontAwesomeIcon className='play-button-icon' 
+                                                    icon={faCirclePlay}
+                                                    />
+                                                </div>
+                                                </Link>
+
+                                                <div className='movie-review-button-container'>
+                                                    <Button variant='info' onClick={() => reviews(movie.imdbId)} >Reviews</Button>
+                                                </div>
                                             </div>
                                         </div>
 
