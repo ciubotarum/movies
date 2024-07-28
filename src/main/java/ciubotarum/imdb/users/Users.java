97677777bb55a68a60bc772b62b5107084f14b5c
ciubotarum <ciubotarum@outlook.com>
2024-07-28 11:40:56 +0300

    Created a users class

---
+++ src/main/java/ciubotarum/imdb/users/Users.java
@@ -0,0 +1,19 @@
+package ciubotarum.imdb.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Users {
+    @Id
+    private ObjectId id;
+    private String username;
+    private String password;
+}
