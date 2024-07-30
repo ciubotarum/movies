081a464ec8a9b4f28b358b3b0b91b1f847ad7a9e
ciubotarum <ciubotarum@outlook.com>
2024-07-30 10:37:18 +0300

    Added login functionality

--- src/main/java/ciubotarum/imdb/users/UserController.java
+++ src/main/java/ciubotarum/imdb/users/UserController.java
@@ -3,10 +3,7 @@ package ciubotarum.imdb.users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -16,8 +13,13 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/create")
+    @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody Map<String, String> payload) {
-        return new ResponseEntity<>(userService.register(payload.get("username"), payload.get("id")), HttpStatus.CREATED);
+        return new ResponseEntity<>(userService.register(payload.get("username"), payload.get("password")), HttpStatus.CREATED);
     }
+    @GetMapping("/login")
+    public ResponseEntity<User> login(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<>(userService.login(payload.get("username"), payload.get("password")), HttpStatus.OK);
+    }
+
 }
