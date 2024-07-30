e4aca10313485e3c24e9611f8ed65a8bbe3503d7
ciubotarum <ciubotarum@outlook.com>
2024-07-30 10:38:14 +0300

    Added new methods

--- src/main/java/ciubotarum/imdb/users/UserRepository.java
+++ src/main/java/ciubotarum/imdb/users/UserRepository.java
@@ -4,4 +4,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
+    boolean existsByUsername(String username);
+    boolean existsByPassword(String password);
 }
