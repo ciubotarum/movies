6f1a77a98678a29ceafc3a157e1d94cb62d1f342
ciubotarum <ciubotarum@outlook.com>
2024-08-01 15:34:37 +0300

    Added handle submit function

--- MovieClient/movie-gold-v1/src/components/login/Login.js
+++ MovieClient/movie-gold-v1/src/components/login/Login.js
@@ -1,16 +1,52 @@
 import '../../components/register/RegisterPage.css';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('/api/v1/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        });
+
+        if (response.ok) {
+            // Handle successful login
+            const user = await response.json();
+            console.log('Login successful:', user);
+            // Clear error message
+            setError('');
+            // Redirect to home page
+            navigate('/');
+        } else {
+            // Handle login error
+            setError('Username or password is incorrect');
+            console.error('Login failed');
+        }
+    };
+
+
     return (
-        <div class="registration-form">
+        <div className="registration-form">
             <h1>Login</h1>
-            <form action="#" method="post">
+            {error && <p className='error'>{error}</p>}
+            <form onSubmit={handleSubmit}>
                 <p>User Name:</p>
-                <input type="text" name="username" placeholder="User "></input>
+                <input type="text" name="username" placeholder="User" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                 <p>Password:</p>
-                <input type="text" name="password" placeholder="Password "></input>
-                <button  type="submit">Register</button>
+                <input type="text" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
+                <button  type="submit">Login</button>
             </form>
         </div>
 
