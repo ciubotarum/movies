a45c5ce484baff237088612f27daf0c4e2758aef
ciubotarum <ciubotarum@outlook.com>
2024-08-01 19:32:10 +0300

    Added functionality to add new users from frontend

--- MovieClient/movie-gold-v1/src/components/register/RegisterPage.js
+++ MovieClient/movie-gold-v1/src/components/register/RegisterPage.js
@@ -1,18 +1,63 @@
-import React from "react";
 import './RegisterPage.css';
+import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import api from '../../api/axiosConfig';
 
 const RegisterPage = () => {
 
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (password !== confirmPassword) {
+            setError("Passwords do not match");
+            return;
+        }
+
+        try {
+            const response = await api.post('/api/v1/users/register', {
+                username: username,
+                password: password
+            });
+
+            if (response.status === 201) {
+                // Handle successful login
+                const user = response.data;
+                console.log('Registered successful:', user);
+                // Clear error message
+                setError('');
+                // Redirect to home page
+                navigate('/login');
+            }
+        } catch (error) {
+            // Handle login error
+            if (error.response) {
+                setError(error.response.data.message || "Registration failed");
+                console.error('Register failed', error.response.data);
+            } else {
+                setError('Registration failed');
+                console.error('Register failed', error);
+            }
+        }
+    };
+
     return (
-        <div class="registration-form">
+        <div className="registration-form">
             <h1>Registration Form</h1>
-            <form action="#" method="post">
+            {error && <p className='error'>{error}</p>}
+            <form onSubmit={handleSubmit}>
                 <p>User Name:</p>
-                <input type="text" name="username" placeholder="User "></input>
+                <input type="text" name="username" placeholder="User" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                 <p>Password:</p>
-                <input type="text" name="password" placeholder="Password "></input>
+                <input type="text" name="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 <p> Validate Password:</p>
-                <input type="text" name="password" placeholder="Password "></input>
+                <input type="text" name="confirmPassword" placeholder="Confirm Password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)}></input>
                 <button  type="submit">Register</button>
             </form>
         </div>
