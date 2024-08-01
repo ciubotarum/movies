fc509b8986bc37f643a777fa34ddcd894dc789e2
ciubotarum <ciubotarum@outlook.com>
2024-08-01 16:59:31 +0300

    Added a new method to find by username and password

--- src/main/java/ciubotarum/imdb/users/UserRepository.java
+++ src/main/java/ciubotarum/imdb/users/UserRepository.java
@@ -5,5 +5,8 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
     boolean existsByUsername(String username);
-    boolean existsByPassword(String password);
+
+    boolean existsByUsernameAndPassword(String username, String password);
+
+    User getByUsername(String username);
 }
