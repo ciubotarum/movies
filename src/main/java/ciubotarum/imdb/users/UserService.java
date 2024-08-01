5f3f7acd7efc78a07d62d93b4170709c9e151c1b
ciubotarum <ciubotarum@outlook.com>
2024-08-01 17:01:16 +0300

    Solved login issue

--- src/main/java/ciubotarum/imdb/users/UserService.java
+++ src/main/java/ciubotarum/imdb/users/UserService.java
@@ -9,23 +9,23 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class UserService {
     @Autowired
-    UserRepository userRepository;
+    private UserRepository userRepository;
 
     public User register(String username, String password) {
-         if (StringUtil.isNullOrEmpty(username) || StringUtil.isNullOrEmpty(password)){
-             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Empty username or password");
-         }
-         if (userRepository.existsByUsername(username)) {
-             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This username already exists");
-         }
-        return userRepository.insert(new User(username, password));
+        if (StringUtil.isNullOrEmpty(username) || StringUtil.isNullOrEmpty(password)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Empty username or password");
+        }
+        if (userRepository.existsByUsername(username)) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This username already exists");
+        }
+        return userRepository.save(new User(username, password));
     }
 
     public User login(String username, String password) {
-        if (userRepository.existsByUsername(username) && userRepository.existsByPassword(password)) {
-            throw new ResponseStatusException(HttpStatus.OK, "You're successfully logged in");
+        if(!userRepository.existsByUsernameAndPassword(username, password)){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong username or password");
         }
 
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Your username or password do not match!");
+        return userRepository.getByUsername(username);
     }
 }
