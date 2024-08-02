632961490eaf4d6fd3e94feea00177ea0e009592
ciubotarum <ciubotarum@outlook.com>
2024-08-02 15:35:14 +0300

    Added password encoder in login and register methods

--- src/main/java/ciubotarum/imdb/users/UserService.java
+++ src/main/java/ciubotarum/imdb/users/UserService.java
@@ -3,6 +3,7 @@ package ciubotarum.imdb.users;
 import ch.qos.logback.core.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -11,6 +12,9 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User register(String username, String password) {
         if (StringUtil.isNullOrEmpty(username) || StringUtil.isNullOrEmpty(password)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Empty username or password");
@@ -18,11 +22,14 @@ public class UserService {
         if (userRepository.existsByUsername(username)) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This username already exists");
         }
-        return userRepository.save(new User(username, password));
+        String encodedPassword = passwordEncoder.encode(password);
+        User user = new User(username, encodedPassword); // Ensure you use proper user model
+        return userRepository.save(user);
     }
 
     public User login(String username, String password) {
-        if(!userRepository.existsByUsernameAndPassword(username, password)){
+        String encodedPassword = passwordEncoder.encode(password);
+        if(!userRepository.existsByUsernameAndPassword(username, encodedPassword)){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Wrong username or password");
         }
 
